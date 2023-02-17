f0a13f68557f202452f5ad2d1ce6fe76b14b1d7d
malliaamy <104427003+malliaamy@users.noreply.github.com>
2023-02-17 21:49:19 +0100

    Initial Commit
    + Created the project
    + free asset that will be replaced later on
    + Endless moving mechanic 
    + Single jump

---
+++ EndlessRunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D myRigidbody; 
+
+
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    private Collider2D myCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+        myCollider = GetComponent<Collider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if(grounded)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            }
+        }
+    }
+}
